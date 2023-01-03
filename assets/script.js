f2a5ad377ba255b76953f0d6a5e947718f277a08
AlbertoPaim <119620474+AlbertoPaim@users.noreply.github.com>
2023-01-03 18:22:54 -0300

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+
+// BOTÃO MENU
+function clicou() {
+
+const menu = document.querySelector(".menu2")
+
+if (menu.style.display == "none") {
+menu.style.display="flex"       
+} 
+
+else {
+    menu.style.display="none"  
+}
+
+}
+
+// cadastro
+
+function cadastro() {
+
+    alert("CADASTRO FEITO COM SUCESSO")
+}
